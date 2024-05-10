7b4de4daabb7a854cb5cfd8f304bb8482b641b25
caritogcdev <carito940905@gmail.com>
2024-05-09 23:01:44 -0500

    Configuration and use of swagger.

--- querer-tour-api/src/main/java/com/riwi/querertourapi/api/controllers/TownController.java
+++ querer-tour-api/src/main/java/com/riwi/querertourapi/api/controllers/TownController.java
@@ -3,11 +3,17 @@ package com.riwi.querertourapi.api.controllers;
 import com.riwi.querertourapi.api.dto.request.TownRequest;
 import com.riwi.querertourapi.api.dto.response.TownResponse;
 import com.riwi.querertourapi.infrastructure.abstract_services.ITownService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.function.EntityResponse;
 
@@ -15,11 +21,17 @@ import org.springframework.web.servlet.function.EntityResponse;
 @RestController
 @RequestMapping("/town")
 @AllArgsConstructor
+/** Con Tag podemos cambiar el nombre en Swagger*/
+@Tag(name = "Towns") // Usando el Swagger
 public class TownController {
 
     @Autowired
     private final ITownService iTownService;
 
+    @Operation( // Usando el Swagger
+            summary = "Lista todos los pueblos con paginación",
+            description = "Debes enviar la página y el tamaño de la página para recibir u obtener todos los pueblos correspondientes."
+    )
     @GetMapping
     /** Estamos respondiendo una entidad que va a tener como
      * genérico un page que tiene como genérico un TownResponse
@@ -43,6 +55,22 @@ public class TownController {
      *
      * Para traer ese id, se hace con la notación @PathVariable
      */
+
+    //ApiResponse nos ayuda a crear un nuevo esquema (dto) de respuesta
+    @ApiResponse(
+            responseCode = "400",
+            description = "Cuando el id no es válido",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema =  @Schema(implementation = ErrorResponse.class)
+                    )
+            }
+    )
+    @Operation( // Usando el Swagger
+            summary = "Lista un pueblo por ID",
+            description = "Debes enviar el id del pueblo a buscar, para obtener el pueblo correspondiente."
+    )
     @GetMapping(path = "/{id}")
     public ResponseEntity<TownResponse> getById(@PathVariable String id){
         return ResponseEntity.ok(this.iTownService.getById(id));
@@ -50,6 +78,11 @@ public class TownController {
 
     /** Controlador que llama al servicio para crear registros
      * de Town en la DB */
+
+    @Operation( // Usando el Swagger
+            summary = "Crea un pueblo",
+            description = "Debes enviar los valores correspondientes para crear un pueblo"
+    )
     @PostMapping
     public ResponseEntity<TownResponse> insert(
             @Validated /** Uso de la librería validation, siempre va antes del @RequestBody */
@@ -57,6 +90,20 @@ public class TownController {
         return ResponseEntity.ok(this.iTownService.create(town));
     }
 
+    @ApiResponse(
+            responseCode = "400",
+            description = "Cuando el id no es válido",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema =  @Schema(implementation = ErrorResponse.class)
+                    )
+            }
+    )
+    @Operation( // Usando el Swagger
+            summary = "Elimina un pueblo por id",
+            description = "Debes enviar el id del pueblo a eliminar."
+    )
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id){
 
@@ -66,6 +113,20 @@ public class TownController {
         return ResponseEntity.noContent().build();
     }
 
+    @ApiResponse(
+            responseCode = "400",
+            description = "Cuando el id no es válido",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema =  @Schema(implementation = ErrorResponse.class)
+                    )
+            }
+    )
+    @Operation( // Usando el Swagger
+            summary = "Actualiza un pueblo por id",
+            description = "Actualiza un pueblo por id"
+    )
     @PutMapping(path = "/{id}")
     public ResponseEntity<TownResponse> update(
             @PathVariable String id, /** id por url */
--- querer-tour-api/src/main/java/com/riwi/querertourapi/config/OpenApiConfig.java
+++ querer-tour-api/src/main/java/com/riwi/querertourapi/config/OpenApiConfig.java
@@ -1,4 +1,21 @@
 package com.riwi.querertourapi.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Aquí configuramos la librería de
+ * swagger
+ * */
+@Configuration // Para configurar beans dentro de spring
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Api para administración de los pueblos.",
+                version = "1.0",
+                description = "Esta API fue creada para el proyecto QuerertouR, donde se está aprendiendo acerca de los fundamentos de Spring Boot y Spring JPA."
+        )
+)
 public class OpenApiConfig {
+
 }
